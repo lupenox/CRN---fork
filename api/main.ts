3124d441364427207846289b7c653cec97e53483
Adam <147440973+adam-polak@users.noreply.github.com>
2026-02-13 14:46:54 -0600

    Update main.ts

--- api/main.ts
+++ api/main.ts
@@ -22,26 +22,26 @@ Deno.serve(async (req) => {
     });
   }
 //TODO call db function
-// if (req.method === "PATCH" && req.url.endsWith("/update-resource")) {
-//   try {
-//     const { id, field, value } = await req.json();
-//     const updatedResource = await updateResourceField(id, field, value);
-//     return new Response(JSON.stringify({success: true, data: updatedResource}), {
-//       headers: { 
-//         "Content-Type": "application/json", 
-//         "Access-Control-Allow-Origin": "*" 
-//       },
-//     });
-//   } catch (err) {
-//     return new Response(JSON.stringify({ success: false, error: String(err) }), {
-//       status: 400,
-//       headers: { 
-//         "Content-Type": "application/json", 
-//         "Access-Control-Allow-Origin": "*" 
-//       },
-//     });
-//   }
-// }
+if (req.method === "PATCH" && req.url.endsWith("/update-resource")) {
+  try {
+    const { id, field, value } = await req.json();
+    // TODO call db function
+    return new Response(JSON.stringify({success: true, data: updatedResource}), {
+      headers: { 
+        "Content-Type": "application/json", 
+        "Access-Control-Allow-Origin": "*" 
+      },
+    });
+  } catch (err) {
+    return new Response(JSON.stringify({ success: false, error: String(err) }), {
+      status: 400,
+      headers: { 
+        "Content-Type": "application/json", 
+        "Access-Control-Allow-Origin": "*" 
+      },
+    });
+  }
+}
 
   const result = await sql`SELECT 1 AS ok`;
   return new Response(JSON.stringify(result[0]), {
