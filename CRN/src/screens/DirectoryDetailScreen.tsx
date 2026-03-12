17482ec3839622f10a27b514fba46659277765c6
conno <cjleberg@uwm.edu>
2026-03-12 11:19:51 -0500

    change: changed pages to more uniform scheme, DirectoryDetailScreen now just says Location details instead of repeating title

--- CRN/src/screens/DirectoryDetailScreen.tsx
+++ CRN/src/screens/DirectoryDetailScreen.tsx
@@ -19,7 +19,7 @@ export default function DirectoryDetailScreen({ route, navigation })
     }
     return (
         <Layout level="2" style={{ flex: 1 }}>
-          <AppHeader title={event.title} showBack={true} />
+            <AppHeader title="Location Details" showBack={true} />
             <ScrollView>
                 <Card style={{ paddingVertical: 12 }}>
                 <Text category="h5" style={{ marginBottom: 6 }}>{event.title}</Text>
