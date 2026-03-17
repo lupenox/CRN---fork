2e4f247d4d88138782e844b68b5c695320f73e93
HawkerX <jackharmer5@gmail.com>
2026-03-17 18:33:04 -0500

    feat: added basic dynamic delete endpoint

--- api/routes/dynamic.ts
+++ api/routes/dynamic.ts
@@ -1,4 +1,9 @@
-import { getDynamicData, postDynamicData } from "../util/dynamicDb.ts";
+import { 
+	getDynamicData, 
+	postDynamicData,
+	delDynamicData
+} from "../util/dynamicDb.ts";
+
 import CrnResponse from "../util/httpResponse.ts";
 
 export async function GET(req : Request) {
@@ -75,6 +80,38 @@ export async function POST(req : Request) {
             null,
             'Table is not in the allow list',
             { status: 401 }
+        );
+    }
+
+    return CrnResponse(result, null);
+}
+
+export async function DELETE(req: Request){
+    const url = new URL(req.url);
+    const id = parseInt(
+        url.searchParams.get('id') ?? ''
+    );
+    const table = url.searchParams.get('table');
+
+    if(table == null) {
+        return CrnResponse(
+            null,
+            'Table must be specified in query params',
+            { status: 400 }
+        );
+    }
+
+    let result;
+    try {
+        if(Number.isInteger(id)) {
+            result = await delDynamicData(table, id);
+        }
+	} catch(err) {
+        console.log(err);
+        return CrnResponse(
+            null,
+            'Failed to delete record',
+            { status: 500 }
         );
     }
 
